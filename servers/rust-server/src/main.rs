d23077033d18695c6c9aadafe54e28f7cf56e34d
Nignik <maksymilian.olbrys@gmail.com>
2025-09-17 17:31:48 +0200

    Improved testing, and extended messages.proto handling

--- servers/rust-server/src/main.rs
+++ servers/rust-server/src/main.rs
@@ -1,21 +1,25 @@
 mod connection_handler;
 
-use std::sync::Arc;
 use connection_handler::ConnectionHandler;
+use std::sync::Arc;
 use tokio::net::TcpListener;
 
-pub mod comm {
+pub mod comm
+{
   include!(concat!(env!("OUT_DIR"), "/emdr_messages.rs"));
 }
 
 #[tokio::main]
-async fn main() {
+async fn main()
+{
   let args: Vec<String> = std::env::args().collect();
-  let (host, port) = match args.len() {
+  let (host, port) = match args.len()
+  {
     1 => ("127.0.0.1".to_string(), 4000),
     2 => ("127.0.0.1".to_string(), args[1].parse().unwrap_or(4000)),
     3 => (args[1].clone(), args[2].parse().unwrap_or(4000)),
-    _ => {
+    _ =>
+    {
       println!("Usage: {} [host] [port]", args[0]);
       return;
     }
@@ -27,13 +31,16 @@ async fn main() {
 
   let conn_handler = Arc::new(ConnectionHandler::default());
 
-  while let Ok((stream, _)) = listener.accept().await {
+  while let Ok((stream, _)) = listener.accept().await
+  {
     let handler = conn_handler.clone();
 
     tokio::spawn(async move {
-      let conn_id = match handler.accept_connection(stream).await {
+      let conn_id = match handler.accept_connection(stream).await
+      {
         Ok(id) => id,
-        Err(e) => {
+        Err(e) =>
+        {
           println!("Failed with {} to accept connection with", e);
           return;
         }
